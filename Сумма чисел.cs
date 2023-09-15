4e6240a0596fed1e0642253a3935207499021605
iliyamad <145149312+iliyamad@users.noreply.github.com>
2023-09-15 20:10:25 +0300

    Add files via upload

---
+++ Сумма чисел.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Text
+{
+ class Program
+{
+    static void Main(string[]args)
+    {
+    Console.WriteLine("Ведите первое число: ")
+    int number1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Ведите второе число: ")
+    int number2 = int.Parse(Console.ReadLine());
+    Console.WriteLine(number1 + number2)
+    
+        
+    }
+}
+}
